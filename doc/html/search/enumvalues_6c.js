b6a4148d94bdc9d76d7d7ccd9ecdeb3ad0d73783
Peter Hutterer <peter.hutterer@who-t.net>
2013-07-03 15:28:56 +1000

    Documentation update for bacb8e2
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- doc/html/search/enumvalues_6c.js
+++ doc/html/search/enumvalues_6c.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['libevdev_5fforce_5fsync',['LIBEVDEV_FORCE_SYNC',['../libevdev_8h.html#a91c53c94148902faf9391bb4aa320299ab2b7a16634fc66bdee1d4b600ed3ee52',1,'libevdev.h']]],
   ['libevdev_5fgrab',['LIBEVDEV_GRAB',['../libevdev_8h.html#a8ed340121b7f4d56223dff90b964a010ad3ac6f5f3ebf7d38a6aad74a88396c88',1,'libevdev.h']]],
+  ['libevdev_5fread_5fblocking',['LIBEVDEV_READ_BLOCKING',['../libevdev_8h.html#a91c53c94148902faf9391bb4aa320299a093c57956b416da7ff70e52669570ffa',1,'libevdev.h']]],
   ['libevdev_5fread_5fnormal',['LIBEVDEV_READ_NORMAL',['../libevdev_8h.html#a91c53c94148902faf9391bb4aa320299a7d73c32e0a017a2816dfeff0b8f7343b',1,'libevdev.h']]],
   ['libevdev_5fread_5fsync',['LIBEVDEV_READ_SYNC',['../libevdev_8h.html#a91c53c94148902faf9391bb4aa320299a7f050f41c99aaffc5e19a43316a9d6cd',1,'libevdev.h']]],
   ['libevdev_5fungrab',['LIBEVDEV_UNGRAB',['../libevdev_8h.html#a8ed340121b7f4d56223dff90b964a010a6c0930d0c280753504cd05ebdcda09eb',1,'libevdev.h']]]
