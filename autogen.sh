929705cb7b25df17c21235b5b921a67ef3afb23f
Giovanni Campagna <gcampagn@redhat.com>
2013-09-09 14:41:10 +0200

    Support GNOME Build API
    By adding an autogen.sh file with NOCONFIGURE support.
    
    Signed-off-by: Giovanni Campagna <scampa.giovanni@gmail.com>
    Reviewed-by: David Herrmann <dh.herrmann@gmail.com>
    Reviewed-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+
+cd "$srcdir"
+test -d m4 || mkdir m4
+autoreconf -fvi || exit $?
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || ./configure "$@"
